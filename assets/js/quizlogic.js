f4515bca80c7123f447c99ebfb45691703a22077
Kal O <koestreicher4@gmail.com>
2022-06-22 06:30:52 -0500

    added js

---
+++ assets/js/quizlogic.js
@@ -0,0 +1,72 @@
+// array of questions + answers
+
+
+var Questions = [
+    {
+        questionText: "Commonly used data types do NOT include:",
+        correctAnswer: "",
+        answerArr: [
+            'Numbers',
+            'strings',
+            'Boolean',
+            'alerts'
+        ]
+
+    }
+    var Questions = [
+        {
+            questionText: "Commonly used data types do NOT include:",
+            correctAnswer: "",
+            answerArr: [
+                'Numbers',
+                'strings',
+                'Boolean',
+                'alerts'
+            ]
+    
+        }
+        var Questions = [
+            {
+                questionText: "Commonly used data types do NOT include:",
+                correctAnswer: "",
+                answerArr: [
+                    'Numbers',
+                    'strings',
+                    'Boolean',
+                    'alerts'
+                ]
+        
+            }
+            var Questions = [
+                {
+                    questionText: "Commonly used data types do NOT include:",
+                    correctAnswer: "",
+                    answerArr: [
+                        'Numbers',
+                        'strings',
+                        'Boolean',
+                        'alerts'
+                    ]
+            
+                }
+                var Questions = [
+                    {
+                        questionText: "Commonly used data types do NOT include:",
+                        correctAnswer: "",
+                        answerArr: [
+                            'Numbers',
+                            'strings',
+                            'Boolean',
+                            'alerts'
+                        ]
+                
+                    }
+    
+            
+
+
+
+
+
+
+]
